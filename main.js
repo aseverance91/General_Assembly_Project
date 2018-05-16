62c6c261c1ee0ef94ff4167f4d890ccdb3a93f5e
aseverance91 <34077376+aseverance91@users.noreply.github.com>
2018-05-16 15:33:32 -0400

    updates

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready (function (){
+
+
+const anchorElTwo = document.querySelector('.click-button-two');
+
+anchorElTwo.addEventListener('click',event => {
+	var xhr = $.get("https://api.giphy.com/v1/gifs/random?tag=mom&api_key=X3SR3WWlYTW8tzghpaqqhIWjF83h0vJc");
+	xhr.done(function(data) {console.log(data.data.title); $("#ourImage").attr("src",data.data.images.original.url)});
+
+	console.log(xhr)
+});
+
+const anchorEl = document.querySelector('.click-button');
+
+anchorEl.addEventListener('click',event => {
+	var xhrl = $.get("https://api.giphy.com/v1/gifs/random?tag=Trump&api_key=X3SR3WWlYTW8tzghpaqqhIWjF83h0vJc");
+	xhrl.done(function(data) {console.log(data.data.title); $("#ourImage2").attr("src",data.data.images.original.url)});
+	
+	console.log(xhrl)
+});
+
+});
+
+
